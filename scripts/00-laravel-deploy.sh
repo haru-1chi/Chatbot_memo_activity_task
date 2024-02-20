8742ddb8e29de47c3ca74dc4877bd023a7e2fed6
haru-1chi <jarunee.recc2@gmail.com>
2024-02-20 18:59:55 +0700

    update schedule run

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,23 +2,35 @@
 echo "Running composer"
 composer dump-autoload
 composer install --no-dev --working-dir=/var/www/html
+
+# Set permissions
+echo "Setting permissions..."
 chmod -R 777 /var/www/html/storage/logs
 
-echo "Caching optimize..."
+# Clear optimization
+echo "Clearing optimization..."
 php artisan optimize:clear
 
-echo "Caching config..."
+# Cache configuration
+echo "Caching configuration..."
 php artisan config:cache
 
+# Cache routes
 echo "Caching routes..."
 php artisan route:cache
 
+# Run migrations
 echo "Running migrations..."
 php artisan migrate --force
 
 # Set up cron job for Laravel scheduler
 echo "Setting up cron job for Laravel scheduler..."
-echo "* * * * * cd /var/www/html && php artisan schedule:run >> /dev/null 2>&1" > /etc/cron.d/laravel-scheduler
+echo "* * * * * cd /var/www/html && php artisan schedule:run >> /var/www/html/cron.log 2>&1" > /var/www/html/cron-job
+
+# Install the cron job
+sudo mv /var/www/html/cron-job /etc/cron.d/laravel-scheduler
+
+echo "Deployment complete."
 
 #echo "Running seeders..."
 #php artisan db:seed
